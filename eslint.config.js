97abd0803c54c75d63393df1b3135f78868bff2e
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-08-07 14:07:26 +0300

    Initial commit

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,11 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist/**/*',
+    '.idea/**/*',
+    'node_modules/**/*',
+  ]),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -15,9 +19,39 @@ export default tseslint.config([
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.app.json',
+      },
+    },
+    
+    rules: {
+      // TypeScript strictness
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'no-unused-vars': 'off',
+      
+      // React/JSX
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      
+      // Optional: stylistic rules
+      'quotes': ['warn', 'single', { avoidEscape: true }],
+      'semi': ['warn', 'never'],
     },
   },
 ])
--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { StarterPage } from '~/components'
 import './App.css'
 
 function App() {
-  
   return (
     <>
+      {/* test lint*/}
       <StarterPage />
     </>
   )
