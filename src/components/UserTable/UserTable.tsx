2c7880ebc42c0f308e701feb97135f3a2ca4db31
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-08-08 18:51:41 +0300

    fix(user table): wrap rows with <TableBody> and add <Table> to fix invalid DOM nesting

--- src/components/UserTable/UserTable.tsx
+++ src/components/UserTable/UserTable.tsx
@@ -1,4 +1,4 @@
-import { Paper, TableContainer } from '@mui/material'
+import { Paper, Table, TableContainer } from '@mui/material'
 import { UserTableBody } from '~/components/UserTable/UserTableBody.tsx'
 import { UserTableHead } from '~/components/UserTable/UserTableHead.tsx'
 import type { User } from '~/schemas'
@@ -6,8 +6,10 @@ import type { User } from '~/schemas'
 export const UserTable = ({ users }: { users: User[] }) => {
   return (
     <TableContainer component={Paper}>
-      <UserTableHead />
-      <UserTableBody users={users} />
+      <Table>
+        <UserTableHead />
+        <UserTableBody users={users} />
+      </Table>
     </TableContainer>
   )
 }
--- src/components/UserTable/UserTableBody.tsx
+++ src/components/UserTable/UserTableBody.tsx
@@ -1,12 +1,13 @@
+import { TableBody } from '@mui/material'
 import { memo } from 'react'
 import { UserRow } from '~/components/UserTable/UserRow.tsx'
 import type { User } from '~/schemas'
 
 export const UserTableBody = memo(({ users }: { users: User[] }) => (
-  <>
+  <TableBody>
     {users.map((user) => (
       <UserRow key={user.login.uuid}
                user={user} />
     ))}
-  </>
+  </TableBody>
 ))
