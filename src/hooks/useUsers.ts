691e3aa85f7fc698d50a3945a41485f14718d073
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-08-08 16:23:17 +0300

    feat(user): add useUsers hook

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useState } from 'react'
+import type { User } from '~/schemas'
+import { userService } from '~/services'
+
+export const useUsers = (
+  page: number,
+  gender?: string,
+  nat?: string,
+) => {
+  const [users, setUsers] = useState<User[]>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  
+  const loadUsers = useCallback(async () => {
+      try {
+        setLoading(true)
+        setError('')
+        const data = await userService.getUsers(page, gender, nat)
+        setUsers(data)
+      } catch (err) {
+        console.error(err)
+        setError('Failed to fetch users.')
+        setUsers([])
+      } finally {
+        setLoading(false)
+      }
+    },
+    [page, gender, nat],
+  )
+  
+  useEffect(() => {
+    (async () => await loadUsers())()
+  }, [loadUsers])
+  
+  return { users, loading, error }
+}
